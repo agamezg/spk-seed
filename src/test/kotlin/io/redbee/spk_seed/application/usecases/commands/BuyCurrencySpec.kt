ae03f070cc77a58be6d88b74296b85e7f1e1dff6
fpontecorvo <fabrizio.pontecorvo@redb.ee>
2022-10-17 16:32:13 -0300

    test

--- src/test/kotlin/io/redbee/spk_seed/application/usecases/commands/BuyCurrencySpec.kt
+++ src/test/kotlin/io/redbee/spk_seed/application/usecases/commands/BuyCurrencySpec.kt
@@ -3,62 +3,16 @@ package io.redbee.spk_seed.application.usecases.commands
 import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.testCoroutineScheduler
 import io.mockk.*
 import io.redbee.spk_seed.application.ports.out.CurrencyCache
 import io.redbee.spk_seed.application.ports.out.CurrencyMessenger
 import io.redbee.spk_seed.application.ports.out.CurrencyRepository
 import io.redbee.spk_seed.modelTest.aValidBuyCommand
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.days
 
-class BuyCurrencySpec : BehaviorSpec({
-
-    lateinit var currencyRepository: CurrencyRepository
-    lateinit var currencyMessenger: CurrencyMessenger
-    lateinit var currencyCache: CurrencyCache
-    lateinit var buyCurrency: BuyCurrency
-
-    beforeTest {
-        currencyRepository = mockk()
-        currencyMessenger = mockk()
-        currencyCache = mockk()
-        buyCurrency = BuyCurrency(
-            currencyRepository = currencyRepository,
-            currencyMessenger = currencyMessenger,
-            currencyCache = currencyCache
-        )
-    }
-
-    Given("a valid buy command") {
-        val aBuyCommand = aValidBuyCommand
-        And("mock dependencies") {
-            coEvery {
-                currencyRepository.save(aBuyCommand.currency)
-            } returns aBuyCommand.currency
-
-            coEvery {
-                currencyMessenger.publish(aBuyCommand)
-            } returns Unit
-
-            coEvery {
-                currencyCache.put(aBuyCommand.currency)
-            } returns true
-        }
-
-        When("call a execute method") {
-            runBlocking { buyCurrency.execute(aBuyCommand) }
-        }
-
-        Then("verify calls") {
-            verify(exactly = 1) { runBlocking { currencyMessenger.publish(aBuyCommand) } }
-            verify(exactly = 1) { runBlocking { currencyMessenger.publish(aBuyCommand) } }
-            verify(exactly = 1) { runBlocking { currencyCache.put(aBuyCommand.currency) } }
-        }
-    }
-
-
-})
-
-
+@OptIn(ExperimentalStdlibApi::class, kotlinx.coroutines.ExperimentalCoroutinesApi::class)
 class BuyCurrencySpec2 : FunSpec({
 
     lateinit var currencyRepository: CurrencyRepository
@@ -77,8 +31,9 @@ class BuyCurrencySpec2 : FunSpec({
         )
     }
 
-    test("sdasdas").config(coroutineTestScope = true) {
+    test("buy currency successfully").config(coroutineTestScope = true) {
         val aBuyCommand = aValidBuyCommand
+        val duration = 1.days
 
         coEvery {
             currencyRepository.save(aBuyCommand.currency)
@@ -95,9 +50,10 @@ class BuyCurrencySpec2 : FunSpec({
 
         launch { buyCurrency.execute(aBuyCommand) }
 
+        testCoroutineScheduler.advanceTimeBy(duration.inWholeMilliseconds)
 
-        coVerify(exactly = 1) { currencyRepository.save(aBuyCommand.currency) }
         coVerify(exactly = 1) { currencyMessenger.publish(aBuyCommand) }
+        coVerify(exactly = 1) { currencyRepository.save(aBuyCommand.currency) }
         coVerify(exactly = 1) { currencyCache.put(aBuyCommand.currency) }
 
     }
