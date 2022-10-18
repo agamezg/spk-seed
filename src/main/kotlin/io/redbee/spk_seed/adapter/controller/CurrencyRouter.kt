4ff101efa273ec49ec813880bbadb6b6bc0e36b0
fpontecorvo <fabrizio.pontecorvo@redb.ee>
2022-10-18 10:50:18 -0300

    benchmark

--- src/main/kotlin/io/redbee/spk_seed/adapter/controller/CurrencyRouter.kt
+++ src/main/kotlin/io/redbee/spk_seed/adapter/controller/CurrencyRouter.kt
@@ -3,16 +3,15 @@ package io.redbee.spk_seed.adapter.controller
 import io.redbee.spk_seed.application.ports.`in`.BuyCommand
 import io.redbee.spk_seed.application.ports.`in`.BuyCurrencyCommand
 import io.redbee.spk_seed.application.ports.`in`.GetCurrenciesQuery
+import io.redbee.spk_seed.shared.CompanionLogger
+import io.redbee.spk_seed.shared.benchmark
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.map
 import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
-import org.springframework.web.reactive.function.server.bodyToFlow
+import org.springframework.web.reactive.function.server.*
 
 @Component
 class CurrencyRouter(
@@ -20,9 +19,11 @@ class CurrencyRouter(
     val buyCurrencyCommand: BuyCurrencyCommand
 ) {
 
+    companion object : CompanionLogger()
+
     @Bean
     fun coRouter() =
-        org.springframework.web.reactive.function.server.coRouter {
+        coRouter {
             (accept(MediaType.APPLICATION_JSON) and "/api/v2/currencies").nest {
                 POST("", ::buyCurrency)
                 GET("", ::getCurrencies)
@@ -30,16 +31,21 @@ class CurrencyRouter(
         }
 
     private suspend fun buyCurrency(serverRequest: ServerRequest) =
-        serverRequest
-            .bodyToFlow<BuyCommand>()
-            .map { buyCurrencyCommand.execute(it) }
-            .toServerResponse()
+        log.benchmark("buyCurrency") {
+            serverRequest
+                .bodyToFlow<BuyCommand>()
+                .map { buyCurrencyCommand.execute(it) }
+                .toServerResponse()
+        }
+
 
     private suspend fun getCurrencies(serverRequest: ServerRequest) =
-        getCurrenciesQuery
-            .execute()
-            .asFlow()
-            .toServerResponse()
+        log.benchmark("getCurrencies") {
+            getCurrenciesQuery
+                .execute()
+                .asFlow()
+                .toServerResponse()
+        }
 
     private suspend inline fun <reified T : Any> Flow<T>.toServerResponse() =
         ServerResponse
---
+++ src/main/kotlin/io/redbee/spk_seed/shared/benchmark.kt
@@ -0,0 +1,34 @@
+package io.redbee.spk_seed.shared
+
+import reactor.util.Logger
+import org.slf4j.MDC
+
+data class BenchmarkResult<T>(val response: T, val elapsedTime: Long)
+
+
+/**
+ * It receives a block, executes it and returns the result of the block with the elapsed time.
+ * @param block
+ * @return @see BenchmarkResult<T>
+ */
+inline fun <T> measureTimeMillisWithResponse(block: () -> T): BenchmarkResult<T> {
+  val start = System.currentTimeMillis()
+  val response = block()
+  return BenchmarkResult(response, System.currentTimeMillis() - start)
+}
+
+/**
+ * Receives a block.
+ * Logs the start with @param actionName and executes the block.
+ * Logs the end and return block result.
+ * @param actionName
+ * @param block
+ */
+inline fun <T> Logger.benchmark(actionName: String, block: () -> T): T {
+  info("Start execution of {}", actionName)
+  val result = measureTimeMillisWithResponse(block)
+  info("End execution of {} - elapsed time {} ms", actionName, result.elapsedTime)
+  MDC.put("elapsed_time", result.elapsedTime.toString())
+  return result.response
+}
+
